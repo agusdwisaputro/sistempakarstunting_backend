17c79828f42a2b96ab9b7afa35c9ef9bbe818d04
agusdwisaputro <agusd4386@gmail.com>
2025-06-23 23:17:02 +0700

    Update konfigurasi database di .env dan db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
+import 'dotenv/config'; // untuk baca file .env
 import { Sequelize } from "sequelize";
 
-export const db = new Sequelize("sistem_pakar", "root", "agus123", {
-  host: "localhost",
-  port: "3306",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 export default db;
